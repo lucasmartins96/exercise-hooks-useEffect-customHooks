3181138efe77f53a71f85666ff81fa524833dc07
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-24 10:03:00 -0300

    refatorando o componente Selector

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,32 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Consumer } from './RedditContext';
 
-const renderOptions = (options) => (
+const renderOptions = (options) =>
   options.map((option) => (
-    <option
-      value={option}
-      key={option}
-    >
+    <option value={option} key={option}>
       {option}
     </option>
-  ))
-);
+  ));
 
-const Selector = () => (
-  <Consumer>
-    {({ selectedSubreddit, availableSubreddits, selectSubreddit }) => (
-      <span>
-        <h1>{`Selected: ${selectedSubreddit}`}</h1>
-        <select
-          onChange={(e) => selectSubreddit(e.target.value)}
-          value={selectedSubreddit}
-        >
-          {renderOptions(availableSubreddits)}
-        </select>
-      </span>
-    )}
-  </Consumer>
-);
+const Selector = () => {
+  const { selectedSubreddit, availableSubreddits, selectSubreddit } =
+    useContext(Consumer);
+
+  return (
+    <span>
+      <h1>{`Selected: ${selectedSubreddit}`}</h1>
+      <select onChange={(e) => selectSubreddit(e.target.value)} value={selectedSubreddit}>
+        {renderOptions(availableSubreddits)}
+      </select>
+    </span>
+  );
+};
 
 export default Selector;
